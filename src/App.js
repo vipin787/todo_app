e5185d79ee4e686643502769359aec115f34593f
vipin <cvipin787@gmail.com>
2023-09-21 23:38:47 +0530

    Add functionality for add, delete and update items

--- src/App.js
+++ src/App.js
@@ -1,55 +1,102 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import uuid from "react-uuid";
-import TodoList from './components/TodoList';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import TodoList from "./components/TodoList";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [item, setItem] = useState("");
   const [itemList, setItemList] = useState([]);
+  const [toggleBtn, setToggleBtn] = useState(false);
+  const [itemId, setItemId] = useState();
 
-  function handleChange (e) {
+  function handleChange(e) {
     console.log(e.target.value);
     setItem(e.target.value);
   }
 
-  function addItem () {
-    const itemObj = {id: uuid(), itemName: item}
-    setItemList((prevItem)=> [...prevItem, itemObj]);
-    // console.log([...prevItem], item);
-    toast.success("Item added successfully")
+  function addItem() {
+    if (toggleBtn) {
+      const newList = itemList.map((todo) => {
+        if (todo.id === itemId) {
+          return { ...todo, itemName: item };
+        }
+        return todo;
+      });
+      setItemList(newList);
+      setToggleBtn(false);
+      setItem("");
+      setItemId();
+      toast.info("Item Updated Successfully")
+    } else {
+      const itemObj = { id: uuid(), itemName: item };
+      setItemList((prevItem) => [...prevItem, itemObj]);
+      // console.log([...prevItem], item);
+      toast.success("Item added successfully");
+    }
   }
 
-  function deleteItem (id) {
+  function deleteItem(id) {
     console.log(id);
     const filterItem = itemList.filter((value) => {
-      return value.id !== id 
+      return value.id !== id;
     });
     setItemList(filterItem);
-    toast.error("Item deleted successfully")
+    toast.error("Item deleted successfully");
   }
 
-  function deleteAll () {
+  function deleteAll() {
     setItemList([]);
-    toast.error("All item deleted successfully")
+    toast.error("All item deleted successfully");
+  }
+
+  function editItem(id) {
+    // console.log(id)
+    const editTodo = itemList.find((todo) => {
+      return todo.id === id;
+    });
+    console.log("id =>", id);
+    setItem(editTodo.itemName);
+    setToggleBtn(true);
+    setItemId(id);
   }
 
   return (
     <div className="App">
-      <div className="parent-div" >
-        <h1 style={{border: "1px solid gray", borderRadius: "5px"}}>React To-Do List App</h1>
+      <div className="parent-div">
+        <h1 style={{ border: "1px solid gray", borderRadius: "5px" }}>
+          React To-Do List App
+        </h1>
         <div className="upper-half">
           <div className="input-div">
-            <input type="search" value={item} onChange={handleChange} placeholder="Todo..." />
+            <input
+              type="search"
+              value={item}
+              onChange={handleChange}
+              placeholder="Todo..."
+            />
           </div>
           <div className="btn-div">
-            <button className="add-btn" onClick={addItem} disabled={item<=0 ? true: false}>Add Item</button>
-            <button className="delete-btn" onClick={deleteAll}>Delete All </button>
+            <button
+              className="add-btn"
+              onClick={addItem}
+              disabled={item <= 0 ? true : false}
+            >
+              {" "}
+              {toggleBtn ? "Update Item" : "Add Item"}
+            </button>
+            <button className="delete-btn" onClick={deleteAll}>
+              Delete All{" "}
+            </button>
           </div>
         </div>
         <div className="lower-half">
-          <TodoList itemList={itemList} deleteItem={deleteItem} />
+          <TodoList
+            itemList={itemList}
+            deleteItem={deleteItem}
+            editItem={editItem}
+          />
           <ToastContainer theme="dark" />
           {/* theme="colored" and normal also */}
         </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./TodoList.css";
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 
-function TodoList({ itemList, deleteItem }) {
+function TodoList({ itemList, deleteItem, editItem }) {
   return (
     <>
       {itemList.length !== 0 ?
       itemList.map((val) => {
         return (
-          <div className="item">
+          <div className="item" key={val.id}>
             <div className="item-child">
               <span>{val.itemName}</span>
               <div className="btns">
                 <div>
-                  <AiOutlineEdit style={{ cursor: "pointer" }} onClick={()=>deleteItem(val.id)} />
+                  <AiOutlineEdit style={{ cursor: "pointer" }} onClick={()=>editItem(val.id)} />
                 </div>
                 <div>
-                  <AiOutlineDelete style={{ cursor: "pointer" }} />
+                  <AiOutlineDelete style={{ cursor: "pointer" }} onClick={()=> deleteItem(val.id)} />
                 </div>
               </div>
             </div>
